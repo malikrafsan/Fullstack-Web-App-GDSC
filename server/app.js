f4b180068a9d341c5d263b227dc591b26b4c00d2
Malik Akbar Hashemi Rafsanjani <pro.malikakbar2357@gmail.com>
2022-02-02 12:17:56 +0700

    feat: connect to supabase database

--- server/app.js
+++ server/app.js
@@ -1,11 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { createClient } = require("@supabase/supabase-js");
+
+const supabaseUrl = "https://ickjmznffvkgvbiylfrx.supabase.co";
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+app.get("/", async (req, res) => {
+  const { data, error } = await supabase.from("wishlist").select(`
+    id, user_id, movie_id
+  `);
+
+  console.log(data);
+  console.log(error);
+
+  res.send(error ? error : data);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
