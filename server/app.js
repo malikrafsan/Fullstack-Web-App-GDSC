0b1464b0f4f2fee70ed448c7db60376ae6e230a9
Malik Akbar Hashemi Rafsanjani <pro.malikakbar2357@gmail.com>
2022-02-02 08:42:30 +0700

    feat: add login, register, and get movies functionality

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 const port = 3000;
 
 const dotenv = require("dotenv");
@@ -12,14 +13,62 @@ const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 app.get("/", async (req, res) => {
-  const { data, error } = await supabase.from("wishlist").select(`
-    id, user_id, movie_id
+  const { data, error } = await supabase.from("movie").select(`
+    *
   `);
 
   console.log(data);
-  console.log(error);
-
   res.send(error ? error : data);
+});
+
+app.post("/login", async (req, res) => {
+  const { data, error } = await supabase
+    .from("user")
+    .select(
+      `
+    username, password
+  `
+    )
+    .eq("username", req.body?.username)
+    .eq("password", req.body?.password);
+
+  if (error) {
+    res.status(500).send(error);
+  }
+
+  if (data !== []) {
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    res.status(404).send("Missing username or password");
+  }
+
+  const { data, error } = await supabase
+    .from("user")
+    .select("username")
+    .eq("username", req.body?.username);
+
+  if (data.length > 0) {
+    res.status(403).send("Username already exists");
+  } else {
+    const { data, error } = await supabase.from("user").insert({
+      username: req.body?.username,
+      password: req.body?.password,
+    });
+
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.status(200).send(data);
+    }
+  }
 });
 
 app.listen(port, () => {
